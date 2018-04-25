3ae7054ba74c9f3d34e9495fb1f354c658e9443c
robson <robhenrod@gmail.com>
2018-04-24 23:19:22 -0300

    Sass syntax upgrade

---
+++ src/js/smoothscroll-config.js
@@ -0,0 +1,24 @@
+SmoothScroll({                
+    // Scrolling Core
+    frameRate        : 300, // [Hz]
+    animationTime    : 1500, // [ms]
+    stepSize         : 120, // [px]
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm   : true,
+    pulseScale       : 8,
+    pulseNormalize   : 1,
+
+    // Acceleration
+    accelerationDelta : 20,  // 50
+    accelerationMax   : 1,   // 3
+
+    // Keyboard Settings
+    keyboardSupport   : true,  // option
+    arrowScroll       : 50,    // [px]
+
+    // Other
+    fixedBackground   : true, 
+    excluded          : ''
+});
