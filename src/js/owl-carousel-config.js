f91b9b962dd021b32ddad38cb90dacf293accc8d
robson <robhenrod@gmail.com>
2018-05-14 22:07:30 -0300

    Blog section

--- src/js/owl-carousel-config.js
+++ src/js/owl-carousel-config.js
@@ -1,8 +1,25 @@
-$('.owl-carousel').owlCarousel({
+$('.testimonial-carousel').owlCarousel({
     loop: true,
     responsive: {
         0: {
             items: 1
         }
     }
-})
+});
+
+$('.blog-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1024: {
+            items: 3
+        }
+    }
+});
